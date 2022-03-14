d17256715f80150afe06f23d9454468a6325a880
coSmoBoY9 <bekhruzallaberganov@gmail.com>
2022-03-14 13:20:51 +0500

    start lession: function signatures

---
+++ src/signatures.ts
@@ -0,0 +1,4 @@
+// function signatures
+/***
+ * Function signature - bu funksiyaning umumiy deklaratsiyasining bir qismi bo'lib, tarjima vositalariga aynan shu funksiyani boshqalar qatorida aniqlash imkonini beradi. Turli xil dasturlash tillarida imzo ( imzo ) haqida turli xil fikrlar mavjud.
+ */
